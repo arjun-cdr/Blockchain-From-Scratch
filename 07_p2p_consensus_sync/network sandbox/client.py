56100850e3258d4bc738e808b967d9d13fe4576d
Arjunaditya Das <adityaarjun434@gmail.com>
2026-07-18 22:40:50 +0530

    updated client.py

--- 07_p2p_consensus_sync/network sandbox/client.py
+++ 07_p2p_consensus_sync/network sandbox/client.py
@@ -1,1 +1,8 @@
+import requests
 
+print("Sending requests to server...")
+response = requests.get('http://127.0.0.1:5000/')
+
+print("Connection successsful...")
+print("Status Code : ", response.status_code)
+print("Data Received : ", response.json())
