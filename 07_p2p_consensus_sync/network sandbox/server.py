6e48491541a0ac25d75c73d947124e143bf9fc7d
Arjunaditya Das <adityaarjun434@gmail.com>
2026-07-20 22:17:37 +0530

    updated server_2.py

--- 07_p2p_consensus_sync/network sandbox/server.py
+++ 07_p2p_consensus_sync/network sandbox/server.py
@@ -12,6 +12,12 @@ def add_to_server():
 @app.route("/")
 def return_data():
     return {"data received": mempool}
+
+@app.route("/sync")
+def sync_peers():
+    target = "http://127.0.0.1:5001/add"
+    response = request.post(target, json={"pet":mempool})
+    return {"status":"Server Broadcast complete"}
     
 if __name__ ==  "__main__":
     app.run(host = '127.0.0.1',port=5000, debug = True)
