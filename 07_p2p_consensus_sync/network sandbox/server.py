c07e181b0686d4fac4dd33e0b95fb427714666b5
Arjunaditya Das <adityaarjun434@gmail.com>
2026-07-23 00:13:50 +0530

    updated server.py

--- 07_p2p_consensus_sync/network sandbox/server.py
+++ 07_p2p_consensus_sync/network sandbox/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+
 app = Flask(__name__)
 mempool = []
 
@@ -7,7 +8,7 @@ mempool = []
 def add_to_server():
     incoming_data = request.get_json()
     mempool.append(incoming_data)
-    return {"status" : "Successfully received data from Client..."} 
+    return {"status" : "Successfully received data from Server 2 (Node B)"} 
     
 @app.route("/")
 def return_data():
@@ -16,8 +17,17 @@ def return_data():
 @app.route("/sync")
 def sync_peers():
     target = "http://127.0.0.1:5001/add"
-    response = requests.post(target, json={"pet":mempool})
-    return {"status":"Server Broadcast complete"}
+    response = requests.post(target, json={"data":mempool})
+    if response.status_code==200:
+        return {
+        "status":"Server Broadcast complete",
+        "node_2_reply": response.json(),
+        }
+    else:
+        return{
+        "status":"Server Broadcast Incomplete",
+        "status_code": response.status_code(),
+        }
     
 if __name__ ==  "__main__":
     app.run(host = '127.0.0.1',port=5000, debug = True)
