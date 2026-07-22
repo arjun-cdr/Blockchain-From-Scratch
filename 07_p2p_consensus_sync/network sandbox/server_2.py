1dad44bb0421ae4351f5b4a3bb9042ee6bc6535d
Arjunaditya Das <adityaarjun434@gmail.com>
2026-07-23 00:14:22 +0530

    updated server_2.py

--- 07_p2p_consensus_sync/network sandbox/server_2.py
+++ 07_p2p_consensus_sync/network sandbox/server_2.py
@@ -7,11 +7,11 @@ mempool = []
 def add_to_server():
     incoming_data = request.get_json()
     mempool.append(incoming_data)
-    return {"status" : "Successfully received data from Client..."} 
+    return {"status" : "Successfully received data from Server 1 (Node A)"} 
     
 @app.route("/")
 def return_data():
     return {"data received": mempool}
     
 if __name__ ==  "__main__":
-    app.run(host = '127.0.0.1',port=5000, debug = True)
+    app.run(host = '127.0.0.1',port=5001, debug = True)
