23a010b8c3d7f5717ed1de949a64a38d57ec3ad1
Arjunaditya Das <adityaarjun434@gmail.com>
2026-07-23 00:06:06 +0530

    updated server.py

--- 07_p2p_consensus_sync/network sandbox/server.py
+++ 07_p2p_consensus_sync/network sandbox/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-
+import requests
 app = Flask(__name__)
 mempool = []
 
@@ -16,7 +16,7 @@ def return_data():
 @app.route("/sync")
 def sync_peers():
     target = "http://127.0.0.1:5001/add"
-    response = request.post(target, json={"pet":mempool})
+    response = requests.post(target, json={"pet":mempool})
     return {"status":"Server Broadcast complete"}
     
 if __name__ ==  "__main__":
