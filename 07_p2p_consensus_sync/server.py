7cfe7280f88276e6c8d248532ba41d489a1f74d3
Arjunaditya Das <adityaarjun434@gmail.com>
2026-07-28 23:20:28 +0530

    added a chain validator method

--- 07_p2p_consensus_sync/server.py
+++ 07_p2p_consensus_sync/server.py
@@ -24,6 +24,21 @@ class Block:
             self.nonce += 1
             self.hash = self.calculate_hash()
 
+# Chain Validator
+def is_chain_valid(chain_to_check):
+        for i in range(1, len(chain_to_check)):
+            current_block = chain_to_check[i]
+            previous_block = chain_to_check[i-1]
+            
+            # Rule 1: Check if the chain link is broken
+            if current_block["previous_hash"] != previous_block["hash"]:
+                return False
+            
+            # Rule 2: Check if Proof of Work was actually done
+            if not current_block["hash"].startswith(difficulty):
+                return False
+        return True
+
 # Initialize Flask
 app = Flask(__name__)
 
